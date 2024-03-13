ad2cdcec51fc010f8250e8412977719fd1d8fcb3
linux-hzh <3098014138@qq.com>
2024-03-13 15:20:29 +0800

    Site updated: 2024-03-13 15:20:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/12/hello-world/","2024/03/13/我的第一篇博客/"];function toRandomPost(){
+var posts=["2024/03/12/hello-world/","2024/03/13/解决Tomcat服务器启动乱码问题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
