c1c21e890168335fd285aad864f386ff2e87e571
linux-hzh <3098014138@qq.com>
2024-03-13 15:16:39 +0800

    Site updated: 2024-03-13 15:16:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/13/我的第一篇博客/","2024/03/12/hello-world/"];function toRandomPost(){
+var posts=["2024/03/12/hello-world/","2024/03/13/我的第一篇博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
