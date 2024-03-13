ae68e40d1e538bec46ca970c16da10356319740f
linux-hzh <3098014138@qq.com>
2024-03-13 21:42:46 +0800

    Site updated: 2024-03-13 21:42:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/12/hello-world/","2024/03/13/解决Tomcat服务器启动乱码问题/"];function toRandomPost(){
+var posts=["2024/03/12/hello-world/","2024/03/13/小程序开发环境配置/","2024/03/13/解决Tomcat服务器启动乱码问题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
